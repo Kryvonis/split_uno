94795bf34ae2872a24bf4faca1d730c7650de2a7
Akr <kryvonis.artem@gmail.com>
2025-02-13 22:54:30 -0500

    scroll

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,8 +1,48 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    SCROLL_MODE = SAFE_RANGE,
+    DRAG_SCROLL = SAFE_RANGE,
 };
+
+bool set_scrolling = false;
+// Modify these values to adjust the scrolling speed
+#define SCROLL_DIVISOR_H 8.0
+#define SCROLL_DIVISOR_V 20.0
+
+// Variables to store accumulated scroll values
+float scroll_accumulated_h = 0;
+float scroll_accumulated_v = 0;
+
+// Function to handle mouse reports and perform drag scrolling
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    // Check if drag scrolling is active
+
+    if (set_scrolling) {
+        // Calculate and accumulate scroll values based on mouse movement and divisors
+        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
+        scroll_accumulated_v -= (float)mouse_report.y / SCROLL_DIVISOR_V;
+
+        // Assign integer parts of accumulated scroll values to the mouse report
+        mouse_report.h = (int8_t)scroll_accumulated_h;
+        mouse_report.v = (int8_t)scroll_accumulated_v;
+
+        // Update accumulated scroll values by subtracting the integer parts
+        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
+        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
+
+        // Clear the X and Y values of the mouse report
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+    return mouse_report;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == DRAG_SCROLL && record->event.pressed) {
+        set_scrolling = !set_scrolling;
+    }
+    return true;
+}
 
 enum layer_names {
     _BL,
@@ -13,14 +53,14 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
         KC_CAPS,    KC_F1,   KC_F2,     KC_F3,     KC_F4,          KC_F5,         KC_F6,
-        KC_ESC,     KC_1,    KC_2,      KC_3,      KC_4,           KC_5,          KC_MUTE,
-        KC_TAB,     KC_Q,    KC_W,      KC_E,      KC_R,           KC_T,          KC_Y,
-        KC_LCTL,    KC_A,    KC_S,      KC_D,      KC_F,           KC_G,          KC_LBRC,
+        KC_ESC,     KC_1,    KC_2,      KC_3,      KC_4,           KC_5,          KC_6,
+        KC_TAB,     KC_Q,    KC_W,      KC_E,      KC_R,           KC_T,          KC_WBAK,
+        KC_LCTL,    KC_A,    KC_S,      KC_D,      KC_F,           KC_G,          MT(KC_LGUI,KC_LBRC),
         KC_LSFT,    KC_Z,    KC_X,      KC_C,      KC_V,           KC_B,          TG(_FL),
 
-                             MS_BTN2,   KC_LSCR,   KC_BACKSPACE,   KC_SPACE,      LT(_CL,KC_ENT),
-                                                   KC_DEL,         KC_LALT,       KC_LGUI,
-
+                             MS_BTN2,   DRAG_SCROLL,   KC_BACKSPACE,   KC_SPACE,      LT(_CL,KC_ENT),
+                                                   KC_DEL,         MT(MOD_LALT, DRAG_SCROLL),       KC_LGUI,
+ASDFasdfASDF
 
 
 
