684242ce1eefae0fa03570c02cad4715d2d67ec0
Akr <kryvonis.artem@gmail.com>
2025-02-21 00:58:02 -0500

    wfwd

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
         KC_CAPS,    KC_F1,   KC_F2,     KC_F3,     KC_F4,          KC_F5,         KC_F6,
         KC_ESC,     KC_1,    KC_2,      KC_3,      KC_4,           KC_5,          KC_6,
-        KC_TAB,     KC_Q,    KC_W,      KC_E,      KC_R,           KC_T,          KC_WBAK,
+        KC_TAB,     KC_Q,    KC_W,      KC_E,      KC_R,           KC_T,          KC_MUTE,
         KC_LCTL,    KC_A,    KC_S,      KC_D,      KC_F,           KC_G,          KC_LBRC,
-        KC_LSFT,    KC_Z,    KC_X,      KC_C,      KC_V,           KC_B,          KC_MUTE,
+        KC_LSFT,    KC_Z,    KC_X,      KC_C,      KC_V,           KC_B,          DRAG_SCROLL,
 
-                             KC_WBAK,   DRAG_SCROLL,        MT(MOD_LALT, KC_BACKSPACE),   KC_SPACE,      LT(_CL,KC_ENT),
+                             KC_WBAK,   KC_WFWD,   MT(MOD_LALT, KC_BACKSPACE),   KC_SPACE,      LT(_CL,KC_ENT),
                                                             LT(_FL, KC_DEL),              KC_LALT,       MT(MOD_LGUI,KC_WBAK),
 
 
