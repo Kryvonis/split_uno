7e42e395952b16f43eb6703bd8c88a9a4d4cc169
Akr <kryvonis.artem@gmail.com>
2025-02-21 00:53:05 -0500

    cleanup

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -67,37 +67,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,     KC_6,     KC_7,     KC_8,      KC_9,      KC_0,      KC_EQL,
         KC_TAB,      KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,      KC_MINS,
         KC_RBRC,     KC_H,     KC_J,     KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
-        KC_LCTL,     KC_N,     KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
+        KC_WBAK,     KC_N,     KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
 
         DRAG_SCROLL,  KC_DEL, LT(_CL, KC_BACKSPACE),  LT(_FL,KC_ENT),    MS_BTN1,   MS_BTN2
     ),
+
+
+
     [_FL] = LAYOUT(
-        KC_CAPS,    KC_PGUP,   KC_PGDN,     KC_F3,     KC_F4,           KC_F5,         QK_BOOT,
-        KC_ESC,     KC_1,      KC_2,        KC_3,      KC_4,            KC_5,          KC_6,
-        KC_TAB,     KC_4,      KC_5,        KC_6,      KC_R,            KC_T,          KC_Y,
-        KC_LCTL,    KC_7,      KC_8,        KC_9,      KC_0,            KC_G,          KC_LBRC,
-        KC_LSFT,    KC_Z,      KC_X,        KC_C,      KC_V,            KC_B,          TG(_FL),
+        _______,    KC_PGUP,   KC_PGDN,     KC_F3,     _______,            _______,          QK_BOOT,
+        _______,    KC_1,      KC_2,        KC_3,      _______,            _______,          _______,
+        _______,    KC_4,      KC_5,        KC_6,      _______,            _______,          _______,
+        _______,    KC_7,      KC_8,        KC_9,      KC_0,               _______,          _______,
+        _______,    _______,   _______,     _______,   _______,            _______,          _______,
 
-                               MS_BTN2,     MS_BTN1,   KC_DEL,          KC_SPACE,      KC_ENT,
-                                                       _______,         KC_RALT,       KC_LGUI,
-
-
+                               MS_BTN2,     MS_BTN1,   _______,            _______,       _______,
+                                                       _______,            _______,       _______,
 
 
-        KC_F7,       KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_MUTE,
-        KC_CAPS,     KC_6,     KC_7,     KC_8,      KC_9,      KC_MRWD,      KC_MFFD,
-        KC_TAB,      KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,      KC_MINS,
-        KC_LSFT,     KC_LEFT,     KC_DOWN,     KC_UP,      KC_RIGHT,      KC_SCLN,   KC_QUOT,
-        KC_LCTL,     KC_N,     KC_M,     KC_COMM,   KC_MPLY,    KC_MPRV,   KC_MNXT,
 
-        _______,    KC_BACKSPACE,   _______,  KC_ENT,    KC_VOLD,   KC_VOLU
+
+        _______,        _______,        _______,     _______,       _______,       _______,   _______,
+        _______,        _______,        _______,     _______,       _______,       _______,   _______,
+        _______,        _______,        _______,     _______,       _______,       _______,   _______,
+        _______,        KC_LEFT,        KC_DOWN,     KC_UP,         KC_RIGHT,      _______,   _______,
+        KC_MPRV,        KC_MPLY,        KC_MNXT,     _______,       _______,       KC_NUBS,   _______,
+
+        _______,    _______,   _______,  _______,    KC_VOLD,   KC_VOLU
     ),
+
+
+
     [_CL] = LAYOUT(
-        KC_CAPS,    KC_PGUP,      KC_PGDN,     KC_F3,     KC_F4,           KC_F5,         QK_BOOT,
-        KC_ESC,     KC_1,         KC_2,        KC_3,      KC_4,            KC_5,          KC_6,
-        KC_TAB,     KC_4,         KC_5,        KC_6,      KC_R,            KC_T,          KC_Y,
-        KC_LCTL,    KC_MUTE,      KC_MPLY,     KC_MPRV,   KC_MNXT,         KC_G,          KC_LBRC,
-        KC_LSFT,    KC_Z,         KC_X,        KC_VOLD,   KC_VOLU,         KC_VOLU,       TG(_FL),
+        _______,    _______,      KC_PGDN,     KC_F3,     KC_F4,           KC_F5,         QK_BOOT,
+        _______,     KC_1,         KC_2,        KC_3,      KC_4,            KC_5,          KC_6,
+        _______,     KC_4,         KC_5,        KC_6,      KC_R,            KC_T,          KC_Y,
+        _______,    KC_MUTE,      KC_MPLY,     KC_MPRV,   KC_MNXT,         KC_G,          KC_LBRC,
+        _______,    _______,         _______,        KC_VOLD,   KC_VOLU,         _______,       TG(_FL),
 
                                     MS_BTN2,   MS_BTN1,   KC_DEL,          KC_SPACE,      KC_ENT,
                                                           KC_BACKSPACE,    KC_RALT,       KC_LGUI,
