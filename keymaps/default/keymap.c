c65134eca8c34df9a5a0f714ab1ee0debb836c64
Akr <kryvonis.artem@gmail.com>
2025-04-13 00:34:08 -0400

    LAST

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -52,24 +52,24 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
-        KC_CAPS,    KC_F1,   KC_F2,     KC_F3,     KC_F4,          KC_F5,         KC_F6,
-        KC_ESC,     KC_1,    KC_2,      KC_3,      KC_4,           KC_5,          KC_6,
+        KC_ESC,    KC_F1,   KC_F2,     KC_F3,     KC_F4,          KC_F5,         KC_F6,
+        KC_CAPS,    KC_1,    KC_2,      KC_3,      KC_4,           KC_5,          KC_6,
         KC_TAB,     KC_Q,    KC_W,      KC_E,      KC_R,           KC_T,          KC_MUTE,
         KC_LCTL,    KC_A,    KC_S,      KC_D,      KC_F,           KC_G,          KC_LBRC,
         KC_LSFT,    KC_Z,    KC_X,      KC_C,      KC_V,           KC_B,          DRAG_SCROLL,
 
-                             KC_WBAK,   KC_WFWD,   MT(MOD_LALT, KC_BACKSPACE),   KC_SPACE,      LT(_CL,KC_ENT),
-                                                            LT(_FL, KC_DEL),              KC_LALT,       MT(MOD_LGUI,KC_WBAK),
+                                      KC_WBAK,   KC_WFWD,   KC_LGUI,   KC_SPACE,           LT(_FL,KC_ENT),
+                                                            KC_LALT,   KC_BACKSPACE,       LT(_CL, KC_DEL),
 
 
 
-        KC_F7,       KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_MUTE,
+        KC_F7,       KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_BSLS,
         KC_CAPS,     KC_6,     KC_7,     KC_8,      KC_9,      KC_0,      KC_EQL,
         KC_TAB,      KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,      KC_MINS,
         KC_RBRC,     KC_H,     KC_J,     KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
         KC_WBAK,     KC_N,     KC_M,     KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
 
-        DRAG_SCROLL,  KC_DEL, LT(_CL, KC_BACKSPACE),  LT(_FL,KC_ENT),    MS_BTN1,   MS_BTN2
+        DRAG_SCROLL,  KC_BACKSPACE, LT(_CL, KC_DEL),  LT(_FL,KC_ENT),    MS_BTN1,   MS_BTN2
     ),
 
 
@@ -116,6 +116,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,     KC_LEFT,     KC_DOWN,     KC_UP,      KC_RIGHT,      KC_SCLN,   KC_QUOT,
         KC_LCTL,     KC_N,     KC_M,     KC_COMM,   KC_MPLY,    KC_MPRV,   KC_MNXT,
 
-        _______,    KC_BACKSPACE,   _______,  KC_ENT,    KC_VOLD,   KC_VOLU
+        _______,    _______,   _______,  _______,    KC_VOLD,   KC_VOLU
     ),
 };
